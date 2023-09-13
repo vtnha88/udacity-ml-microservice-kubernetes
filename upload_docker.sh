5954487b4ce1bf4b17aeba76bd60226d0e46e6d4
nha vo <nha.vothanh88@gmail.com>
2023-09-13 17:38:16 +0700

    upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,16 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=vtnha88/ml-api:v1.0.0
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+docker login -u vtnha88
+docker tag ml-api $dockerpath:latest
+echo "Docker ID and Image: $dockerpath"
+
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
